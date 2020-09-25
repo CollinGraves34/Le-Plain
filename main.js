da2929589afcdd1964770c58f8093b43ad7fc0e1
collingraves34 <collin.graves@stu.barren.kyschools.us>
2020-09-25 11:21:43 -0500

    Alpha *Has 3rd Card Height Bug*

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("heart1").addEventListener("click", function() {
+    if (document.getElementById("heart1").innerHTML == '<i class="far fa-heart"></i>'){
+        document.getElementById("heart1").innerHTML = '<i class="fas fa-heart"></i>';
+    }else{
+        document.getElementById("heart1").innerHTML = '<i class="far fa-heart"></i>';
+    }
+});
+
+document.getElementById("heart2").addEventListener("click", function() {
+    if (document.getElementById("heart2").innerHTML == '<i class="far fa-heart"></i>'){
+        document.getElementById("heart2").innerHTML = '<i class="fas fa-heart"></i>';
+    }else{
+        document.getElementById("heart2").innerHTML = '<i class="far fa-heart"></i>';
+    }
+});
+
+document.getElementById("heart3").addEventListener("click", function() {
+    if (document.getElementById("heart3").innerHTML == '<i class="far fa-heart"></i>'){
+        document.getElementById("heart3").innerHTML = '<i class="fas fa-heart"></i>';
+    }else{
+        document.getElementById("heart3").innerHTML = '<i class="far fa-heart"></i>';
+    }
+});
