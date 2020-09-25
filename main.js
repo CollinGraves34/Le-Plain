e297919272653386f6ec85ff3473cab92056b555
collingraves34 <collin.graves@stu.barren.kyschools.us>
2020-09-25 12:46:19 -0500

    Alpha *Fixed Card Bug* & Working Hearts/LikeBTN

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-document.getElementById("heart1").addEventListener("click", function() {
-    if (document.getElementById("heart1").innerHTML == '<i class="far fa-heart"></i>'){
-        document.getElementById("heart1").innerHTML = '<i class="fas fa-heart"></i>';
-    }else{
-        document.getElementById("heart1").innerHTML = '<i class="far fa-heart"></i>';
-    }
-});
+likeBtnHandler();
 
-document.getElementById("heart2").addEventListener("click", function() {
-    if (document.getElementById("heart2").innerHTML == '<i class="far fa-heart"></i>'){
-        document.getElementById("heart2").innerHTML = '<i class="fas fa-heart"></i>';
-    }else{
-        document.getElementById("heart2").innerHTML = '<i class="far fa-heart"></i>';
-    }
-});
 
-document.getElementById("heart3").addEventListener("click", function() {
-    if (document.getElementById("heart3").innerHTML == '<i class="far fa-heart"></i>'){
-        document.getElementById("heart3").innerHTML = '<i class="fas fa-heart"></i>';
-    }else{
-        document.getElementById("heart3").innerHTML = '<i class="far fa-heart"></i>';
-    }
-});
+
+
+
+
+/* Like Button Handle  */
+function likeBtnHandler() {
+    document.getElementById("heart1").addEventListener("click", function() {
+        if (document.getElementById("heart1").innerHTML == '<i class="far fa-heart"></i>'){
+            document.getElementById("heart1").innerHTML = '<i class="fas fa-heart"></i>';
+        }else{
+            document.getElementById("heart1").innerHTML = '<i class="far fa-heart"></i>';
+        }
+    });
+    
+    document.getElementById("heart2").addEventListener("click", function() {
+        if (document.getElementById("heart2").innerHTML == '<i class="far fa-heart"></i>'){
+            document.getElementById("heart2").innerHTML = '<i class="fas fa-heart"></i>';
+        }else{
+            document.getElementById("heart2").innerHTML = '<i class="far fa-heart"></i>';
+        }
+    });
+    
+    document.getElementById("heart3").addEventListener("click", function() {
+        if (document.getElementById("heart3").innerHTML == '<i class="far fa-heart"></i>'){
+            document.getElementById("heart3").innerHTML = '<i class="fas fa-heart"></i>';
+        }else{
+            document.getElementById("heart3").innerHTML = '<i class="far fa-heart"></i>';
+        }
+    });
+}
+/* --------------------------- */
